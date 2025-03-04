815edfd259ad69434b7b6fecbc7e3ced40abeef4
zohnannor <zohnannor@gmail.com>
2025-03-04 19:56:20 +0300

    chapter 195

--- src/constants.ts
+++ src/constants.ts
@@ -31,7 +31,7 @@ export const HEADERS_WIDTH = 150;
 export const LARGE_FONT_SIZE = 500;
 export const SMALL_FONT_SIZE = 45;
 
-const CHAPTERS_TOTAL = 194;
+const CHAPTERS_TOTAL = 195;
 const EPISODES_TOTAL = 12;
 const ARCS_TOTAL = 14;
 const VOLUMES_TOTAL = 20; // last = unreleased
@@ -58,7 +58,7 @@ export const PAGES_PER_CHAPTER_PER_VOLUME = [
     [14, 16, 16, 15, 16, 15, 15, 16, 15, 17, 16],
     [
         18, 16, 17, 17, 18, 17, 16, 15, 15, 15, 17, 15, 14, 16, 15, 17, 15, 13,
-        15,
+        15, 15,
     ],
 ] as const;
 
@@ -275,6 +275,7 @@ export const CHAPTER_PICTURES = [
         'Volume_19_Pochita_Sketch_4',
     ],
     [
+        null,
         null,
         null,
         null,
@@ -530,6 +531,7 @@ export const CHAPTER_NAMES = [
     'How Devils Play',
     'Burning Kiss',
     'Fun School Festival',
+    'Kill Me Tears',
 ] as const;
 
 export const ARC_NAMES = [
@@ -810,6 +812,7 @@ export const CHAPTER_DATES = map(
         'February 5, 2025',
         'February 12, 2025',
         'February 26, 2025',
+        'March 5, 2025',
     ] as const,
     d => new Date(`${d} GMT+9`) // Tokyo timezone
 );
