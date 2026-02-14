e4a5bbac5c514007b128aae61140d91642511c6d
zohnannor <zohnannor@gmail.com>
2026-02-15 02:55:58 +0300

    configure vite's outDir and default server port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'build',
+    },
+    server: {
+        port: 3000,
+    },
     resolve: {
         alias: {
             '@app': path.resolve(__dirname, './src/app'),
